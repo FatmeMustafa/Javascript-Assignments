ec2b9d1964f221267bdce8066ef9c81aec8b2005
FatmeMustafa <fatme.mustafa888@gmail.com>
2018-02-16 14:18:28 +0100

    not fully done!

--- mandatory-web-shop/scripts/index.js
+++ mandatory-web-shop/scripts/index.js
@@ -21,7 +21,7 @@ sectionPart.addEventListener('click', function() {
 
 /*****************Hiding <section> element on PRODUCTS and CHECKOUT*****************/
 
-let listItems = document.getElementsByClassName("hide");     
+let listItems = document.getElementsByClassName("hide");  
 let hiding = function() {     
     document.getElementsByClassName("mySection")[0].style.display = "none"; 
 }
@@ -30,39 +30,80 @@ for (let i = 0; i < listItems.length; i++) {                                    
 }   
 
 
+/*****************Showing <form> element on CHECKOUT*****************/
+
+let showForm = document.getElementById("show2");
+let showing1 = function() {                                   
+    document.getElementById("myForm").style.display = "block";
+}
+showForm.addEventListener("click", showing1);              
+
+/*
+let hideForm = document.getElementsByClassName("hide1");  
+let hiding1 = function() {     
+    document.getElementById("myForm").style.display = "none"; 
+}
+hideForm.addEventListener('click', hiding1);    
+  */    
 
 
+/***************Creating an array of objects representing products*****************/
 
-
-
-
-
-
-
-
-
-
-/*****************Hiding <section> element on PRODUCTS and CHECKOUT*****************/
-
-let products = [
-    {name: "NYX - Velvet Matte", price: 10, description: "lipstick", image: "https://static.pexels.com/photos/33044/sunflower-sun-summer-yellow.jpg"},
-    {name: "NYX - H", price: 20, description: "xxxx", image: "sk"},
-    {name: "NYX - A", price: 20, description: "xxxx", image: "sk"},
-    {name: "NYX - B", price: 20, description: "xxxx", image: "sk"}
+let products = [                                                                  //products variable represents an array which contains 3 objects.
+    {name: "ENVY - ROYAL METALLIC BLUE WITH SILVER ", description: "Lipstick", price: 10, image: "https://www.nyxprofessionalmakeup.pl/dw/image/v2/AAQP_PRD/on/demandware.static/-/Sites-nyx-master-catalog/default/dw8323ec32/ProductImages/Lips/Macaron_Lippies/800897830984_macaronlippies_bluevelvet_swatch_large.jpg?sw=420&sh=420&sm=fit"},
+    {name: "THE LITTLE MERMAID BRUSHES", description: "Brush Set", price: 35, image: "https://ae01.alicdn.com/kf/HTB1MGtbRXXXXXbFXXXXq6xXFXXXc/6pcs-Light-Blue-Mermaid-Makeup-Brush-Set-Fishtail-Cosmetic-Makeup-Brushes-Powder-Concealer-Foundation-Brush-Beauty.jpg"},
+    {name: "RICHE - ROCKET PALETTE", description: "Eye Shadow", price: 10, image: "https://www.lorealparisusa.com/~/media/images/lop/home/products/makeup/eyes/eyeshadow/colour-riche-eye-shadow-quads/avant-garde-azure/cos7u_8.jpg"}
 ];
-document.write(JSON.stringify(products));
+for (x = 0; x < products.length; x++) {                                         //a for-loop is created to loop through the array objects and execute code through each iteration.
+    let newHeading = document.createElement("h3");                              //three <h3> elements are created for the name values.
+    newHeading.id = "h3ID";                                                     //newHeading variable is given an id for styling purposes.  
+    newHeading.style.cssText = "color: #2EFEF7;";
+    let headingContent = document.createTextNode("Product name: " + products[x].name);  //three text nodes are created with a sting plus the three product names.    
+    newHeading.appendChild(headingContent);                                    //places the text nodes inside the <h3> elements. 
+    document.body.appendChild(newHeading);                                     //places the <h3> elements inside the body.
 
-/*let x = document.getElementById("rubyProducts");
-x.addEventListener('click', function() {
-    products.toString();
-    document.getElementById("rubyProducts").innerHTML = products;
-});*/
+    let newHeading1 = document.createElement("h3");
+    newHeading1.id = "h3ID1";                                                       
+    newHeading1.style.cssText = "color: white;";
+    let headingContent1 = document.createTextNode("Description: " + products[x].description);
+    newHeading1.appendChild(headingContent1);
+    document.body.appendChild(newHeading1);
+
+    let newHeading2 = document.createElement("h3");
+    newHeading2.id = "h3ID2"
+    newHeading2.style.cssText = "color: white;";
+    let headingContent2 = document.createTextNode("Price: " + products[x].price);
+    newHeading2.appendChild(headingContent2);
+    document.body.appendChild(newHeading2);
+
+    let newImg = document.createElement("img");
+    newImg.id = "imgID";                       
+    newImg.style.cssText = "width:200px;";                              
+    newImg.setAttribute("src", products[x].image);
+    document.body.appendChild(newImg);
+}
+console.log(products);
 
 
+/***************Checkout*****************/
 
-//for (var x = 0; x < products.length; x++) {
-//    console.log(products);
- //   console.table(products[x]);
-//}
-//rubyProducts.innerHTML = JSON.stringify(products);
+let rubyForm = document.getElementById("submitBTN");
 
+rubyForm.addEventListener("click", () => {
+    let userName = document.getElementById("firstname").value;
+    let userLast = document.getElementById("lastname").value;
+    let userEmail = document.getElementById("emailadress").value;
+    let userPhone = document.getElementById("phonenumber").value;
+    let userStreet = document.getElementById("streetadress").value;
+    let userZip = document.getElementById("zipcode").value;
+    let userCity = document.getElementById("city").value;
+    let userComments = document.getElementById("comments").value;
+
+if (userName == "" || userLast == "" || userEmail == "" || userStreet == "" || userZip == "" || userCity == "") {
+    document.getElementById("error").innerHTML = "Please fill in the required fields marked with *";
+    return false;
+}
+else {
+    document.getElementById("correct").innerHTML = "Thank you!";
+}
+});
