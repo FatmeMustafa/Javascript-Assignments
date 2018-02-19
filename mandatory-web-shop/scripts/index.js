7228742b5a3e9bbfaf00cdb51cf4be348a6ff788
FatmeMustafa <fatme.mustafa888@gmail.com>
2018-02-19 10:39:38 +0100

    klar

--- mandatory-web-shop/scripts/index.js
+++ mandatory-web-shop/scripts/index.js
@@ -1,98 +1,107 @@
 /**************************Showing <section> element on HOME**************************/
+/*****************Hiding <myForm> + <rubyProducts> element on HOME********************/
+let sectionPart = document.getElementById("showHome");                                  //sectionPart variable gets the <li> element with the id "show".
 
-let sectionPart = document.getElementById("show");                                      //sectionPart variable gets the <li> element with the id "show".
 let showing = function() {                                                              //showing function shows all the elements in the class "mySection".
     document.getElementsByClassName("mySection")[0].style.display = "block";            //style of the class "mySection" is set to display block.
+    document.getElementById("myForm").style.display = "none";                           //style of the id "myForm" is set to display none to hide it from HOME.
+    document.getElementById("rubyProducts").style.display = "none";                     //style of the id "rubyProducts" is set to display none to hide it from HOME.
 }
 sectionPart.addEventListener("click", showing);                                         //an EventListener with the showing function is added to the <li> element with the id "show" to show the "mySection" class when clicked.
 
 
-/************************************************************************************/
-/*********************Alternative code - use anonymous function**********************/
-/************************************************************************************/
-
-/*
-let sectionPart = document.getElementById("show");
+/*************************************************************************************/
+/**********************Alternative code - use anonymous function**********************/
+/*************************************************************************************/
+/*let sectionPart = document.getElementById("showHome");
 sectionPart.addEventListener('click', function() {
     document.getElementsByClassName("mySection")[0].style.display = "block";
-});
-*/
+    document.getElementById("myForm").style.display = "none";      
+    document.getElementById("rubyProducts").style.display = "none";    
+});*/
 
 
-/*****************Hiding <section> element on PRODUCTS and CHECKOUT******************/
+/*********************Showing <rubyProducts> element on PRODUCTS**********************/
+/*****************Hiding <myForm> + <section> element on PRODUCTS********************/
+let productPart = document.getElementById("showProducts");              
 
-let listItems = document.getElementsByClassName("hide");  
-let hiding = function() {     
-    document.getElementsByClassName("mySection")[0].style.display = "none"; 
+let showingProduct = function() {                                          
+    document.getElementsByClassName("mySection")[0].style.display = "none";            
+    document.getElementById("myForm").style.display = "none";                           
+    document.getElementById("rubyProducts").style.display = "block";                     
 }
-for (let i = 0; i < listItems.length; i++) {                                        //i variable's start-value = 0, if the array-like-list (listItems) is greater than 0, 1 will be added to i.
-    listItems[i].addEventListener('click', hiding);                                 //an EventListener with the hiding function is added to the two <li> elements with the class name "hide" to hide the "mySection" class when clicked.
-}   
+productPart.addEventListener("click", showingProduct);                                         
 
 
-/*****************Showing <form> element on CHECKOUT*****************/
+/*************************Showing <myForm> element on CHECKOUT************************/
+/**************Hiding <rubyProducts> + <section> element on PRODUCTS******************/
+let checkoutPart = document.getElementById("showForm");              
 
-/*
-let showForm = document.getElementById("show2");
-let showing1 = function() {                                   
-    document.getElementById("myForm").style.display = "block";
+let showingForm = function() {                                          
+    document.getElementsByClassName("mySection")[0].style.display = "none";            
+    document.getElementById("myForm").style.display = "block";                           
+    document.getElementById("rubyProducts").style.display = "none";                     
 }
-showForm.addEventListener("click", showing1);              
-*/
-
-/************Hiding <form> element on HOME and PRODUCTS***********/
-
-/*
-let hideForm = document.getElementsByClassName("hide1");  
-let hiding1 = function() {     
-    document.getElementById("myForm").style.display = "none"; 
-}
-for (let i = 0; i < hide1.length; i++) {                                        
-    hideForm[i].addEventListener('click', hiding1);                                 
-}   
-*/
+checkoutPart.addEventListener("click", showingForm);                                        
 
 
 /***************Creating an array of objects representing products*****************/
-
 let products = [                                                                  //products variable represents an array which contains 3 objects.
     {name: "ENVY - ROYAL METALLIC BLUE WITH SILVER ", description: "Lipstick", price: 10, image: "https://www.nyxprofessionalmakeup.pl/dw/image/v2/AAQP_PRD/on/demandware.static/-/Sites-nyx-master-catalog/default/dw8323ec32/ProductImages/Lips/Macaron_Lippies/800897830984_macaronlippies_bluevelvet_swatch_large.jpg?sw=420&sh=420&sm=fit"},
     {name: "THE LITTLE MERMAID BRUSHES", description: "Brush Set", price: 35, image: "https://ae01.alicdn.com/kf/HTB1MGtbRXXXXXbFXXXXq6xXFXXXc/6pcs-Light-Blue-Mermaid-Makeup-Brush-Set-Fishtail-Cosmetic-Makeup-Brushes-Powder-Concealer-Foundation-Brush-Beauty.jpg"},
     {name: "RICHE - ROCKET PALETTE", description: "Eye Shadow", price: 10, image: "https://www.lorealparisusa.com/~/media/images/lop/home/products/makeup/eyes/eyeshadow/colour-riche-eye-shadow-quads/avant-garde-azure/cos7u_8.jpg"}
 ];
+
+let createDiv = document.createElement("div")
+createDiv.id = "rubyProducts";
+document.body.appendChild(createDiv);
+
 for (x = 0; x < products.length; x++) {                                         //a for-loop is created to loop through the array objects and execute code through each iteration.
     let newHeading = document.createElement("h3");                              //three <h3> elements are created for the name values.
     newHeading.id = "h3ID";                                                     //newHeading variable is given an id for styling purposes.  
     newHeading.style.cssText = "color: #2EFEF7;";
     let headingContent = document.createTextNode("Product name: " + products[x].name);  //three text nodes are created with a sting plus the three product names.    
     newHeading.appendChild(headingContent);                                    //places the text nodes inside the <h3> elements. 
-    document.body.appendChild(newHeading);                                     //places the <h3> elements inside the body.
+    createDiv.appendChild(newHeading);                                         //places the <h3> elements inside the body.
 
     let newHeading1 = document.createElement("h3");
     newHeading1.id = "h3ID1";                                                       
     newHeading1.style.cssText = "color: white;";
     let headingContent1 = document.createTextNode("Description: " + products[x].description);
     newHeading1.appendChild(headingContent1);
-    document.body.appendChild(newHeading1);
+    createDiv.appendChild(newHeading1);
 
     let newHeading2 = document.createElement("h3");
     newHeading2.id = "h3ID2"
     newHeading2.style.cssText = "color: white;";
     let headingContent2 = document.createTextNode("Price: " + products[x].price);
     newHeading2.appendChild(headingContent2);
-    document.body.appendChild(newHeading2);
+    createDiv.appendChild(newHeading2);
 
     let newImg = document.createElement("img");
     newImg.id = "imgID";                       
     newImg.style.cssText = "width:200px;";                              
     newImg.setAttribute("src", products[x].image);
-    document.body.appendChild(newImg);
+    createDiv.appendChild(newImg);
 }
 console.log(products);
 
 
-/***************Checkout*****************/
+/*************************************************************************************/
+/******************************Alternative code - use map*****************************/
+/*************************************************************************************/
+/*let products1 = products.map(product =>                                 //map creates a new array-list by calling a function on the array element products the function returns the <ul> element.            
+    `<ul>                                                               
+        <img src="${product.image}"> 
+        <li>${product.name}</li> 
+        <li>${product.price}</li>
+        <li>${product.description}</li>
+    </ul>`);                                                           //$product.description, price, name etc. returns the values of the array. 
+products1 = products1.join("");                                        //join() joings the elements of products1 into a string and returns the string.
+document.getElementById('rubyProducts').innerHTML = products1;         //places the <ul> element inside the div "rubyProducts".
 
+
+/*************************************Checkout*************************************/
 let rubyForm = document.getElementById("submitBTN");
 rubyForm.addEventListener("click", () => {
     let userName = document.getElementById("firstname").value;
